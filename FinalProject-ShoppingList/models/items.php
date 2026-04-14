597845dca1b77ae4af62d91f6cf8a2d50323da1d
Jack Stuart <jackstuart0426@gmail.com>
2026-04-14 12:38:11 -0400

    Created items model and functions

--- FinalProject-ShoppingList/models/items.php
+++ FinalProject-ShoppingList/models/items.php
@@ -55,4 +55,62 @@ class items {
     ///end getters and setters
 }
 
+function get_items($id) {
+    global $database;
+    $query = 'SELECT id, store_id, name, quantity, checked FROM items WHERE id = :id';
+    $statement = $database->prepare($query);
+    $statement->bindValue(":id", $id);
+    $statement->execute();
+    $items = $statement->fetch();
+    $statement->closeCursor();
+    return new items($items['id'],$items['store_id'],$items['name'],$items['quantity'],$items['checked']);
+}
 
+function list_items() {
+    global $database;
+    $query = 'SELECT id, store_id, name, quantity, checked FROM items';
+    $statement = $database->prepare($query);
+    $statement->execute();
+    $items = $statement->fetchAll();
+    $statement->closeCursor();
+    $items_array = array();
+    foreach ($items as $items) {
+        $items_array[] = new items($items['id'],$items['store_id'],$items['name'],$items['quantity'],$items['checked']);
+    }
+    return $items_array;
+}
+
+function insert_items($items){
+    global $database;
+    $query = "INSERT INTO items (id, store_id, name, quantity, checked) VALUES (:id, :store_id, :name, :quantity, :checked)";
+    $statement = $database->prepare($query);
+    $statement->bindValue(":id", $items->get_id());
+    $statement->bindValue(":store_id", $items->get_store_id());
+    $statement->bindValue(":name", $items->get_name());
+    $statement->bindValue(":quantity", $items->get_quantity());
+    $statement->bindValue(":checked", $items->get_checked());
+    $statement->execute();
+    $statement->closeCursor();        
+}
+
+function update_items($items) {
+    global $database;
+    $query = "UPDATE items SET id = :id, store_id = :store_id, name = :name, quantity = :quantity, checked = :checked";
+    $statement = $database->prepare($query);
+    $statement->bindValue(":id", $items->get_id());
+    $statement->bindValue(":store_id", $items->get_store_id());
+    $statement->bindValue(":name", $items->get_name());
+    $statement->bindValue(":quantity", $items->get_quantity());
+    $statement->bindValue(":checked", $items->get_checked());
+    $statement->execute();
+    $statement->closeCursor();  
+}
+
+function delete_items($items) {
+    global $database;
+    $query = "DELETE FROM items WHERE id = :id";
+    $statement = $database->prepare($query);
+    $statement->bindValue(":id", $items->get_id());
+    $statement->execute();
+    $statement->closeCursor();
+}
